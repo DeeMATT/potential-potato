86e31686cf71e4b058043f918373a1ede25ea035
CodeMasK <mattode@outlook.com>
2021-07-26 00:03:30 +0000

    TestAddition

--- dummy_test/tests.py
+++ dummy_test/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+from .views import add_numbers
+
+
+class TestAddition(unittest.TestCase):
+
+    def test_not_null(self):
+
+        result = add_numbers(10, 5)
+
+        # check if result is None
+        self.assertIsNotNone(result)
+
+    def test_int_addition(self):
+
+        result = add_numbers("love", 2)
+
+        # check if result is an integer
+        self.assertIsInstance(result, int, "Numbers should be an integer")
+
+
+if __name__ == '__main__':
+    unittest.main()
